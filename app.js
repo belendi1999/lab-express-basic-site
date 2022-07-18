cd8c7f432d1eeccc631bf35aad1f7aa8691ceb7a
belendi1999 <97689725+belendi1999@users.noreply.github.com>
2022-07-18 18:34:40 +0200

    rutas creadas y funcionando

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const app = express();
 app.use(express.static('public'));
 app.listen(5400,() => console.log('my first app listening on port 5400'));
 
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/home.html");
+});
 app.get("/about", (req, res) => {
     res.sendFile(__dirname + "/views/about.html");
   });
